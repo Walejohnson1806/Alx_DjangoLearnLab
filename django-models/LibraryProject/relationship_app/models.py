93474d87071ffd3e76968609e29f2cd1e599132a
Wale Johnson <Walejohnson1806@gmail.com>
2025-03-02 16:58:36 +0100

    modified

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -1,6 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+
+
 # Create your models here.
 ROLE_CHOICES = [
     ('Admin', 'Admin'),
@@ -12,6 +17,14 @@ class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     role = models.CharField(max_length=10, choices=ROLE_CHOICES, default="Member")
 
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            UserProfile.objects.create(user=instance, role='Member')
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+        instance.userprofile.save()
 
 class Author(models.Model):
     name = models.CharField(max_length=255)
