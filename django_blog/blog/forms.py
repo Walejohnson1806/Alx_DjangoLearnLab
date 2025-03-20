d9e6ce621cc1ceff7f0e5db20f493ddffb9f5d61
Wale Johnson <Walejohnson1806@gmail.com>
2025-03-20 16:47:25 +0100

    Creating Blog Post Management Features

---
+++ django_blog/blog/forms.py
@@ -0,0 +1,34 @@
+
+from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from .models import Post, Comment
+
+class UserRegisterForm(UserCreationForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
+
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username', 'email']
+        
+class PostForm(forms.ModelForm):
+    
+    class Meta:
+        model = Post
+        fields = ['title', 'content', 'tags']
+        
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['content']
+        widgets = {
+            'content': forms.Textarea(attrs={'rows': 2, 'cols': 20, 'placeholder': 'Leave a comment...'}),
+        }       
+        
