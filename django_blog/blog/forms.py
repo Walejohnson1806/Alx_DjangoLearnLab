8cb653ba17b8720f445e0f73b34264b9dcb926d6
Wale Johnson <Walejohnson1806@gmail.com>
2025-03-26 20:46:03 +0100

    modified

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -1,34 +1,36 @@
 
 from django import forms
-from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
-from .models import Post, Comment
+from django.contrib.auth.models import User
+from .models import Comment, Post, Tag
+from taggit.forms import TagField, TagWidget
+from django.forms import widgets 
 
-class UserRegisterForm(UserCreationForm):
-    email = forms.EmailField()
+class RegisterForm(UserCreationForm):
+    email = forms.EmailField(required=True)
 
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
-
-class UserUpdateForm(forms.ModelForm):
-    email = forms.EmailField()
-
-    class Meta:
-        model = User
-        fields = ['username', 'email']
         
-class PostForm(forms.ModelForm):
-    
-    class Meta:
-        model = Post
-        fields = ['title', 'content', 'tags']
-        
+
+
+
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ['content']
+    
+
+
+
+class PostForm(forms.ModelForm):
+    tags = forms.CharField(widget=TagWidget())  # Use TagWidget for the tags field
+
+    class Meta:
+        model = Post
+        fields = ['title', 'content', 'tags']
         widgets = {
-            'content': forms.Textarea(attrs={'rows': 2, 'cols': 20, 'placeholder': 'Leave a comment...'}),
-        }       
-        
+            'title': widgets.TextInput(attrs={'class': 'form-control'}),
+            'content': widgets.Textarea(attrs={'class': 'form-control'}),
+        }
